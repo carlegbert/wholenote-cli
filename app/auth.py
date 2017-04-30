15dde483562cdbee6869b08b87ec9a6efb30addf
Carl Egbert <egbertcarl@gmail.com>
2017-04-30 11:26:58 -0700

    login request

--- app/auth.py
+++ app/auth.py
@@ -1,8 +1,16 @@
+from base64 import b64encode
 import click
+import requests
 
 
 @click.command()
 @click.option('--email', prompt='email')
 @click.option('--password', prompt='password', hide_input=True)
 def login(email, password):
-    click.echo('Login in with {0}:{1}'.format(email, password))
+    click.echo('Attempting login for {}...'.format(email))
+    empw = '{0}:{1}'.format(email, password)
+    empw_enc = b64encode(empw.encode('utf-8')).decode('utf-8')
+    header = 'Basic {0}'.format(empw_enc)
+    res = requests.post('https://wholenoteapp.com/api/v1.0/login',
+                        headers={'Authorization': header})
+    click.echo(res.json())
